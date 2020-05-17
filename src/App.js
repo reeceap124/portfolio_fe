d1fb2868faa3821f474744f64b554344c7222130
Reece Pierson <piersonreece@gmail.com>
2020-05-17 14:37:24 -0500

    adds okta widget and authentication to work page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import {Route} from 'react-router-dom'
 import './App.scss';
@@ -5,8 +6,9 @@ import Home from './components/Home'
 import About from './components/About'
 import ProjectsPage from './components/projects/ProjectsPage'
 import Contact from './components/Contact'
-import Nav from './components/Nav'
-
+import Nav from './components/Nav';
+import {Security, SecureRoute, LoginCallback} from '@okta/okta-react'
+import config from './components/projects/oktaConfig'
 
 function App() {
   return (
@@ -15,10 +17,14 @@ function App() {
       
       <Route exact path='/' component={Home}/>
       <Route path='/about' component={About}/>
-      <Route path='/work' component ={ProjectsPage}/>
       <Route path='/contact' component={Contact}/>
-      <Route path='/login' />
-      <Route path='/projects'/>
+      <Security {...config}>
+        <Route path='/work' component ={ProjectsPage}/>
+        <Route path='/implicit/callback' component={LoginCallback}/>
+        <SecureRoute path='/admin_work' component={ProjectsPage}/>
+      </Security>
+      
+      
     </div>
   );
 }
--- src/components/projects/ProjectsPage.js
+++ src/components/projects/ProjectsPage.js
@@ -1,9 +1,21 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import ProjectCard from './ProjectsCards';
+import { useOktaAuth } from '@okta/okta-react';
+
 
 const Work = () => {
+    const { authState, authService } = useOktaAuth();
+    const login = () =>authService.login('/admin_work');
     const [projects, setProjects] = useState([])
+    const [admin, setAdmin] = useState(null)
+    useEffect(()=>{
+        if (!authState.isAuthenticated) { 
+            setAdmin(false)
+        } else {
+            setAdmin(true)
+        }
+    }, [authState])
     useEffect(()=>{
         axios.get('http://localhost:3300/api/projects')
         .then(res => {
@@ -18,9 +30,10 @@ const Work = () => {
     return(
         <div className='contentWrapper'>
             <h1>Recent work</h1>
+            {authState.isPending?<p>Loading authentication...</p>:(!authState.isAuthenticated?<div><button type='button' onClick={login}>Login</button></div>:null)}
             <div className='projectsWrapper'>
                 {console.log('projects', projects)}
-                {projects.map(val => <ProjectCard project={val}/>)}
+                {projects.map(val => <ProjectCard project={val} admin={admin}/>)}
             </div>
             
         </div>
